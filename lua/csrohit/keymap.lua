1faa366818b3e67ec786378e0e3d77023c580128
Rohit Nimkar <nehalnimkar@gmail.com>
2023-03-12 06:16:54 +0530

    🔧 add keymap for buffer switch

--- lua/csrohit/keymap.lua
+++ lua/csrohit/keymap.lua
@@ -8,15 +8,19 @@ function Map(mode, lhs, rhs, opts)
     vim.keymap.set(mode, lhs, rhs, options)
 end
 
+-- mapping for normal mode
 function nmap(lhs, rhs, opts)
     Map('n', lhs, rhs, opts)
 end
 
+-- mapping for visual mode
 function vmap(lhs, rhs, opts)
     Map('v', lhs, rhs, opts)
 end
+
 -- set leader key
 vim.g.mapleader = ' '
+vim.g.localmapleader = ' '
 
 -- remove highlighted search results
 nmap('<leader>nh',':nohlsearch<CR>')
@@ -24,6 +28,7 @@ nmap('<leader>e', ':NvimTreeToggle<CR>')
 
 -- navigating between buffers
 nmap('<TAB>', ':bn<CR>', {desc = 'Switch buffers'})
+nmap('S-TAB', ':bp<CR>')
 
 -- navigating between window splits
 nmap('<C-h>', '<C-w>h', { desc = 'Left pane' })
