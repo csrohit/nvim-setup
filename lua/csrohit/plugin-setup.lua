bc6e47ef7aeeeaa5a9c505b826280bdc45996659
Rohit Nimkar <nehalnimkar@gmail.com>
2023-03-12 06:16:30 +0530

    🔧 update module import statement

--- lua/csrohit/plugin-setup.lua
+++ lua/csrohit/plugin-setup.lua
@@ -19,10 +19,7 @@ vim.opt.rtp:prepend(lazypath)
 --
 --  You can also configure plugins after the setup call,
 --    as they will be available in your neovim runtime.
-require('lazy').setup({
-
-    { import = 'csrohit.plugins' }
-})
+require('lazy').setup('csrohit.plugins')
 
 -- stup for nvim-
 vim.g.loaded_netrw = 1
