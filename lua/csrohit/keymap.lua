2a184b715600a0aa742d949d0deb8487a0839162
Rohit Nimkar <nehalnimkar@gmail.com>
2023-03-13 07:49:47 +0530

    ➕ add telescope

--- lua/csrohit/keymap.lua
+++ lua/csrohit/keymap.lua
@@ -43,3 +43,18 @@ nmap('<leader>sm', ':MaximizerToggle<CR>')
 vmap('<', '<gv')
 vmap('>', '>gv')
 
+-- Telescope
+local builtin = require('telescope.builtin')
+nmap('<leader>ff', builtin.find_files, {})
+nmap('<leader>fg', builtin.live_grep, {})
+nmap('<leader>fb', builtin.buffers, {})
+nmap('<leader>fh', builtin.help_tags, {})
+
+-- telescope git commands (not on youtube nvim video)
+nmap("<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
+nmap("<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
+nmap("<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
+nmap("<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
+
+-- restart lsp server (not on youtube nvim video)
+nmap("<leader>rs", ":LspRestart<CR>") -- mapping to restart lsp if necessary
