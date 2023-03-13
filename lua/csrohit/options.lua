0d933e47bc2bb8f874b999bebcd65523a6e65d06
Rohit Nimkar <nehalnimkar@gmail.com>
2023-03-13 16:28:10 +0530

    🔧 add options for cmp

--- lua/csrohit/options.lua
+++ lua/csrohit/options.lua
@@ -53,7 +53,7 @@ vim.o.timeout = true
 vim.o.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
+vim.o.completeopt = 'menu,menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
