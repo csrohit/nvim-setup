56d2c2fad4ea45834ad8527b538b906ee0ab46be
Rohit Nimkar <nehalnimkar@gmail.com>
2023-03-13 07:49:36 +0530

    ➕ add telescope

---
+++ lua/csrohit/plugins/telescope.lua
@@ -0,0 +1,10 @@
+--File: lua/csrohit/plugins/telescope.lua
+
+return {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.1',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'BurntSushi/ripgrep'
+    }
+}
