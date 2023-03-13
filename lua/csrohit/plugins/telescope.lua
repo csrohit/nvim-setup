11f42da50846efb650b392d1966730d76a3fe385
Rohit Nimkar <nehalnimkar@gmail.com>
2023-03-13 09:12:06 +0530

    🔧 add telescope keymap

--- lua/csrohit/plugins/telescope.lua
+++ lua/csrohit/plugins/telescope.lua
@@ -5,6 +5,22 @@ return {
     tag = '0.1.1',
     dependencies = {
         'nvim-lua/plenary.nvim',
-        'BurntSushi/ripgrep'
-    }
+        'BurntSushi/ripgrep',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    },
+    config = function()
+        pcall(require('telescope').load_extension, 'fzf')
+        -- See `:help telescope.builtin`
+        nmap('<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+        nmap('<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+        nmap('<leader>/', function()
+            -- You can pass additional configuration to telescope to change theme, layout, etc.
+            require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+                winblend = 10,
+                previewer = false,
+            })
+        end, { desc = '[/] Fuzzily search in current buffer' })
+
+    end
+
 }
