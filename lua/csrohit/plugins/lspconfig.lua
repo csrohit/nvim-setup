c7b406b652c9f4d120e8c42bc7990d36333f7d9e
Rohit Nimkar <nehalnimkar@gmail.com>
2023-03-16 13:15:46 +0530

    🔧 fix lspsaga kehbindings

--- lua/csrohit/plugins/lspconfig.lua
+++ lua/csrohit/plugins/lspconfig.lua
@@ -17,19 +17,19 @@ local on_attach = function(_, bufnr)
         vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
     end
 
-    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+    -- nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+    -- nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
-    nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+    -- nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+    -- nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
     nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
     nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
     nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
     nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
     -- See `:help K` for why this keymap
-    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-    nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+--    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+--    nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
     -- Lesser used LSP functionality
     nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
@@ -61,7 +61,7 @@ return {
         local mason_lsp = require('mason-lspconfig')
 
         mason_lsp.setup {
-            ensure_installed = { "lua_ls", "clangd", "pyright", 'tsserver', 'gopls' },
+            ensure_installed = { "lua_ls", "clangd", "pyright", 'tsserver' },
         }
 
         -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
--- lua/csrohit/plugins/lspsaga.lua
+++ lua/csrohit/plugins/lspsaga.lua
@@ -2,13 +2,15 @@
 
 return {
     "glepnir/lspsaga.nvim",
-	event = "BufRead",
-	config = function()
-		require("lspsaga").setup({})
-	end,
-	dependencies = {
-		{ "nvim-tree/nvim-web-devicons" },
-		--Please make sure you install markdown and markdown_inline parser
-		{ "nvim-treesitter/nvim-treesitter" }
-	}
+    event = "BufRead",
+    config = function()
+        local saga = require('lspsaga')
+        saga.setup({})
+
+    end,
+    dependencies = {
+        { "nvim-tree/nvim-web-devicons" },
+        --Please make sure you install markdown and markdown_inline parser
+        { "nvim-treesitter/nvim-treesitter" }
+    }
 }
