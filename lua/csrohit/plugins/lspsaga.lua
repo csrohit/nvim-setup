6977e85df60c5611b38c8fb4a24f73c50bab3273
Rohit Nimkar <nehalnimkar@gmail.com>
2023-03-16 09:51:53 +0530

    ➕ add lspsaga

---
+++ lua/csrohit/plugins/lspsaga.lua
@@ -0,0 +1,14 @@
+--File: lua/csrohit/plugins/lspsaga.lua
+
+return {
+    "glepnir/lspsaga.nvim",
+	event = "BufRead",
+	config = function()
+		require("lspsaga").setup({})
+	end,
+	dependencies = {
+		{ "nvim-tree/nvim-web-devicons" },
+		--Please make sure you install markdown and markdown_inline parser
+		{ "nvim-treesitter/nvim-treesitter" }
+	}
+}
