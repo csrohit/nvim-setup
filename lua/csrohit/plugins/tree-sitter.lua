07e1783ec7d9ab53aca29f72648aabe5c5c09542
Rohit Nimkar <nehalnimkar@gmail.com>
2023-03-21 11:15:23 +0530

    🛠️  add additional parsers

--- lua/csrohit/plugins/tree-sitter.lua
+++ lua/csrohit/plugins/tree-sitter.lua
@@ -10,7 +10,7 @@ return {
         -- See `:help nvim-treesitter`
         require('nvim-treesitter.configs').setup {
             -- Add languages to be installed here that you want installed for treesitter
-            ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'help', 'vim' },
+            ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'vim', 'markdown', 'markdown_inline', 'bash', 'gitignore' },
 
             -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
             auto_install = false,
