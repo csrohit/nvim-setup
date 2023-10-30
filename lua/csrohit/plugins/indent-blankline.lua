5de199759e74924bddab2d1e9ae50a7cdb3e5c06
Rohit Nimkar <nehalnimkar@gmail.com>
2023-10-30 13:24:17 +0530

    ⬆ upgrade indent-blenkline to v3

--- lua/csrohit/plugins/indent-blankline.lua
+++ lua/csrohit/plugins/indent-blankline.lua
@@ -5,8 +5,11 @@ return {
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
+    main = "ibl",
     opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
+        indent = {
+            char = '┊',
+        },
+      -- show_trailing_blankline_indent = false,
     },
 }
